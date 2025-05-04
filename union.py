627eb5ed39c864a88bf7b53b4c48d854ee5a42be
THE-1-RV <rudransh1807@gmail.com>
2025-05-04 22:56:29 +0530

    Add files via upload

---
+++ union.py
@@ -0,0 +1,12 @@
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5, 6}
+
+union = E | N           
+intersection = E & N       
+difference = E - N       
+s_difference = E ^ N      
+
+print("Union of E and N is", union)
+print("Intersection of E and N is", intersection)
+print("Difference of E and N is", difference)
+print("Symmetric Difference of E and N is", s_difference) 
